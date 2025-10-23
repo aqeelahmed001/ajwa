4d4d42a07cb3037df0de6acca543bd141b3ea4d6
aqeelahmed001 <info@ajwa.co.jp>
2025-10-23 18:10:36 +0900

    change tiltle

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -17,7 +17,7 @@ export async function generateMetadata({ params }: HomePageProps): Promise<Metad
   
   return {
     title: lang === 'ja' 
-      ? 'アジワ商事 - 機械輸出入' 
+      ? 'アジワ株式会社 - 機械輸出入' 
       : 'Ajwa Co.,LTD - Machinery Export & Import',
     description: lang === 'ja'
       ? '日本を拠点とする機械輸出入会社。中古機械の買取と世界各国への機械輸出を行っております。'
